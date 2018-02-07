9117a18db8cc6f65eb1d3c1f8f33dd79afb4cb26
Bartek Buslowicz <bartek@burin.pl>
2018-02-07 13:53:25 +0100

    Add  final project

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+var stringifyFile;
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    stringifyFile = data
+	    res.send(data);
+	});
+});
+
+
+app.post('/updateNote/:note', function (req, res) {
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+	    if (err) throw err;
+   		stringifyFile = stringifyFile + req.params.note;
+   		console.log('file updated');
+   		res.send(stringifyFile);
+	});
+});
+
+
+
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
